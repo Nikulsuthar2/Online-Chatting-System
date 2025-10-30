094652ba2013e2e83c39b544cf26111fdb5e86c2
Nikul Suthar <98583950+Nikulsuthar2@users.noreply.github.com>
2025-10-30 09:39:03 +0530

    Origin added for netlify frontend

--- Backend/config/allowedOrigin.js
+++ Backend/config/allowedOrigin.js
@@ -1,7 +1,8 @@
 const allowedOrigin = [
     "http://localhost:8000",
     "http://localhost:5173",
-    "http://localhost:4173"
+    "http://localhost:4173",
+    "https://nikschat.netlify.app"
 ];
 
 export default allowedOrigin;
